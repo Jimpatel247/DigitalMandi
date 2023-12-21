c7053dcc16c5e3df5c727b11f935e2ac52724f4b
jimpatel247 <pateljim2002@gmail.com>
2023-12-21 21:55:42 +0530

    * changes

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/images',express.static('images'));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://digital-mandi-amoc.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
